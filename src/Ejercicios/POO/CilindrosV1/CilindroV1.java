68ee67da288a85c6cbdb8f67f996b91bcfcc1f5b
julu <juanluismatalop@gmail.com>
2023-11-10 10:03:47 +0100

    commit 1

---
+++ src/Ejercicios/POO/CilindrosV1/CilindroV1.java
@@ -0,0 +1,49 @@
+package Ejercicios.POO.CilindrosV1;
+
+public class CilindroV1 {
+    private double radio;
+    private double altura;
+
+    public CilindroV1(double radio, double altura) {
+        this.radio = radio;
+        this.altura = altura;
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+    public double CalcularAreaDeLaCircunferencia(){
+        double areaCircunferencia;
+        return areaCircunferencia = 3.1415 * (radio*radio);
+    }
+
+    public double CalcularAreaRectangulo(){
+        double areaRectangulo;
+        return areaRectangulo = altura * (2*radio*3.1415);
+    }
+
+    public double CalcularSuperficie(){
+        return 2*CalcularAreaDeLaCircunferencia()*CalcularAreaRectangulo();
+    }
+
+    public double CalcularVolumenCilindro(){
+        double volumen;
+        return volumen = 3.1415 * (radio*radio)*altura;
+    }
+    @Override
+    public String toString() {
+        return String.format("Los datos de el cilindro de radio %.2f y una altura de %.2f tiene%nVolumen = %.2f%nSuperficie = %.2f%n", getRadio(), getAltura(), CalcularVolumenCilindro(), CalcularSuperficie());
+    }
+}
---
+++ src/Ejercicios/POO/CilindrosV1/TestCilindroV1.java
@@ -0,0 +1,10 @@
+package Ejercicios.POO.CilindrosV1;
+
+import Ejercicios.POO.Cilindros.Cilindro;
+
+public class TestCilindroV1 {
+    public static void main(String[] args) {
+        CilindroV1 cilindroV1 = new CilindroV1(3, 20);
+        System.out.printf(cilindroV1.toString());
+    }
+}
