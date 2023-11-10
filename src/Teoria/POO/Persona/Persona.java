3848b9f93821f63f145fe9084198156fd1ab05ac
julu <juanluismatalop@gmail.com>
2023-11-10 09:54:32 +0100

    commit 1

---
+++ src/Teoria/POO/Persona/Persona.java
@@ -0,0 +1,33 @@
+package Teoria.POO.Persona;
+
+public class Persona {
+    private String nombreCompeeto;
+    private int edad;
+
+    public Persona() {
+        this.nombreCompeeto = nombreCompeeto;
+        this.edad = edad;
+    }
+
+    public String getNombreCompeeto() {
+        return nombreCompeeto;
+    }
+
+    public void setNombreCompeeto(String nombreCompeeto) {
+        this.nombreCompeeto = nombreCompeeto;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%S: %d%n",
+                nombreCompeeto, edad);
+    }
+}
