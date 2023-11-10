2db1e285a203de5104a85dfbe4e3f18a753ef4cd
julu <juanluismatalop@gmail.com>
2023-11-10 09:07:05 +0100

    commit 1

---
+++ src/Ejercicios/POO/EcuacionSegundoGrado/TestEcuacionSegundoGrado.java
@@ -0,0 +1,11 @@
+package Ejercicios.POO.EcuacionSegundoGrado;
+
+public class TestEcuacionSegundoGrado {
+    public static void main(String[] args) {
+        EcuacionSegundoGrado ecuacionSegundoGrado = new EcuacionSegundoGrado();
+        ecuacionSegundoGrado.setA(2);
+        ecuacionSegundoGrado.setB(3);
+        ecuacionSegundoGrado.setC(1);
+        System.out.printf(ecuacionSegundoGrado.toString());
+    }
+}
