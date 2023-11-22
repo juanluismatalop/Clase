2625493f43132d420060642a256bce5103ca7409
julu <juanluismatalop@gmail.com>
2023-11-22 14:17:32 +0100

    22/11/23

---
+++ src/Teoria/Test/Persona.java
@@ -0,0 +1,36 @@
+package Teoria.Test;
+
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private int edad;
+    private String dni;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public Persona(String nombre, String apellido, int edad, String dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.dni = dni;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s, %s, %s, %d", dni, apellido, nombre, edad);
+    }
+}
---
+++ test/Teoria/Test/PersonaTest.java
@@ -0,0 +1,28 @@
+package Teoria.Test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonaTest {
+
+    @Test
+    void getNombre() {
+    }
+
+    @Test
+    void getApellido() {
+    }
+
+    @Test
+    void getEdad() {
+    }
+
+    @Test
+    void getDni() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
