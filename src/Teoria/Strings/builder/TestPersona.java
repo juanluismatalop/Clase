051a3cb6f1901060455dc427ba92ab4ad23e4ce0
julu <juanluismatalop@gmail.com>
2023-12-01 11:23:39 +0100

    1/12/23

---
+++ src/Teoria/Strings/builder/TestPersona.java
@@ -0,0 +1,8 @@
+package Teoria.Strings.builder;
+
+public class TestPersona {
+    public static void main(String[] args) {
+        Persona persona = new Persona("fernandez nuñez", "javier", "12345678", 45);
+        System.out.println(persona.toString());
+    }
+}
---
+++ test/Ejercicios/Cadena/LibreriaCadenas/AuxiliarPalabraTest.java
@@ -0,0 +1,45 @@
+package Ejercicios.Cadena.LibreriaCadenas;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuxiliarPalabraTest {
+
+    @Test
+    void obtenerNumeroDLetras() {
+        assertEquals(-1, AuxiliarPalabra.obtenerNumeroDLetras(null));
+        assertEquals(2, AuxiliarPalabra.obtenerNumeroDLetras("do"));
+        assertEquals(0, AuxiliarPalabra.obtenerNumeroDLetras(""));
+        assertEquals(5, AuxiliarPalabra.obtenerNumeroDLetras("12345"));
+    }
+
+    @Test
+    void empiezaVocal() {
+        assertNull(AuxiliarPalabra.empiezaVocal(null));
+        assertTrue(AuxiliarPalabra.empiezaVocal("amigo"),"amigo");
+        assertFalse(AuxiliarPalabra.empiezaVocal("juan"),"juan");
+        assertTrue(AuxiliarPalabra.empiezaVocal("Emilio"),"Emilio");
+
+    }
+
+    @Test
+    void acabaVocal() {
+        assertNull(AuxiliarPalabra.empiezaVocal(null));
+        assertTrue(AuxiliarPalabra.empiezaVocal("amigo"),"amigo");
+        assertFalse(AuxiliarPalabra.empiezaVocal("juan"),"juan");
+        assertTrue(AuxiliarPalabra.empiezaVocal("Emilio"),"Emilio");
+    }
+
+    @Test
+    void contarVocales() {
+    }
+
+    @Test
+    void contieneLetra() {
+    }
+
+    @Test
+    void esUnPalimodro() {
+    }
+}
